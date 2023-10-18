15cdf5f5ec7b00a8c5d17661691641b92300b640
il94 <ilyes.landolsi@outlook.fr>
2023-10-19 00:17:40 +0200

    feat: enable CORS to localhost:5173
    More security is required

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: 'http://localhost:5173'
+	})
+
+	await app.listen(3000);
 }
 bootstrap();
