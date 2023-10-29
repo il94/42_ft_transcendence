ae1982b141513ee9e0ab2a4a8e0cc7a7ea112068
il94 <ilyes.landolsi@outlook.fr>
2023-10-29 22:44:13 +0100

    feat: add deconnexion and settings buttons (only style)

--- front/src/components/Profile/index.tsx
+++ front/src/components/Profile/index.tsx
@@ -1,9 +1,16 @@
-import { Style } from "./style"
+import { Style, StyledImg, ButtonsWrapper } from "./style"
+
+import deconnexionIcon from "../../assets/deconnexion.png"
+import settingsIcon from "../../assets/settings.png"
 
 function Profile() {
 	return (
 		<Style>
 			Profile
+			<ButtonsWrapper>
+				<StyledImg src={settingsIcon} alt="Settings button" />
+				<StyledImg src={deconnexionIcon} alt="Deconnexion button" />
+			</ButtonsWrapper>
 		</Style>
 	)
 }
--- front/src/components/Profile/style.tsx
+++ front/src/components/Profile/style.tsx
@@ -1,12 +1,46 @@
 import styled from "styled-components"
 
 import colors from "../../utils/colors"
+import effects from "../../utils/effects"
 
 export const Style = styled.div`
 
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 	width: 100%;
-	/* height: 53px; */
-
+	height: 7.75%;
+	min-height: 53px;
+	
 	background-color: ${colors.profile};
 	
+`
+
+export const ButtonsWrapper = styled.div`
+	
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	padding-right: 2.71%;
+
+	width: 36.04%;
+	height: 100%;
+
+`
+
+export const StyledImg = styled.img`
+
+	width: 37px;
+	width: 39.78%;
+
+	cursor: pointer;
+
+	${effects.pixelateIcon};
+	${effects.shadowButton};
+
+
+	border-width: 0.2em; // a definir
+	/* border-width: 2.5px; */
+
 `
