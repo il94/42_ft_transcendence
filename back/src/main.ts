e92c633886505366a9ee33bed71ad55b88be20f6
Claire CHAPON <cchapon@e3r3p1.clusters.42paris.fr>
2023-10-29 18:15:00 +0100

    build: added dockerfile and prisma package -> ko

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,10 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors({
-		origin: 'http://localhost:5173'
-	})
-
+//	app.enableCors({
+//		origin: 'http://localhost:5173'
+//	})
 	await app.listen(3000);
 }
 bootstrap();
