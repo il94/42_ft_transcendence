f0ef6c598e9fff5c3c4a0b86059337e66fbba722
il94 <ilyes.landolsi@outlook.fr>
2023-11-13 17:29:25 +0100

    style: regroup child component folders in parent component folders

--- front/src/components/Social/Friend/style.tsx
+++ front/src/components/Social/Friend/style.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components"
-import colors from "../../utils/colors"
-import effects from "../../utils/effects"
+import colors from "../../../utils/colors"
+import effects from "../../../utils/effects"
 
 export const Style = styled.div`
 
