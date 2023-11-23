2613bda62c9027f17265b34d561b050f547fa7fe
il94 <ilyes.landolsi@outlook.fr>
2023-11-23 02:18:13 +0100

    fix: fix font not instantly rendering

--- front/src/utils/GlobalStyle.tsx
+++ front/src/utils/GlobalStyle.tsx
@@ -3,20 +3,9 @@ import colors from "./colors"
 
 const Style = createGlobalStyle`
 
-	@font-face {
-		font-family: 'RetroGaming';
-		src:
-			url('./src/assets/font/retro_gaming.woff2') format('woff2'),
-			url('./src/assets/font/retro_gaming.woff') format('woff'),
-			url('./src/assets/font/retro_gaming.ttf') format('truetype'),
-			url('./src/assets/font/retro_gaming.eot'),
-			url('./src/assets/font/retro_gaming.eot?#iefix') format('embedded-opentype'),
-			url('./src/assets/font/retro_gaming.svg#svgFontName') format('svg');
-	}
-
 	* {
 		margin: 0;
-		font-family: "RetroGaming";
+		font-family: "Retro Gaming";
 	}
 
 	body {
