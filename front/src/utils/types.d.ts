7cf10f838c8888fb6828824ed5a54f29729d39df
il94 <ilyes.landolsi@outlook.fr>
2023-11-26 23:00:12 +0100

    refactor: add Channel and Messages types

--- front/src/utils/types.d.ts
+++ front/src/utils/types.d.ts
@@ -1,3 +1,5 @@
+import status from "./status"
+
 export type User = {
 	id: number,
 	id42?: number,
@@ -13,4 +15,28 @@ export type User = {
 		draws: number,
 		looses: number
 	}
+}
+
+export type Channel = {
+	id: number,
+	createdAt?: string,
+	name: string,
+	picture: string,
+	type: string
+}
+
+export type Message = {
+	id: number,
+	createdAt?: string,
+	sender: string, // a remplacer par User
+	type: string
+}
+
+export type MessageText = Message & {
+	content: string,
+}
+
+export type MessageInvitation = Message & {
+	target: string // a remplacer par User
+	state: status
 }
