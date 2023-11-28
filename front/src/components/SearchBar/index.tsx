cea6141e75e3ff88b90a5de28dcf74b2c86dcf57
il94 <ilyes.landolsi@outlook.fr>
2023-11-28 15:37:59 +0100

    feat: complet searchBar component

--- front/src/components/SearchBar/index.tsx
+++ front/src/components/SearchBar/index.tsx
@@ -1,22 +1,33 @@
 import Select from "react-select"
 import styled from "styled-components"
+
 import colors from "../../utils/colors"
+import effects from "../../utils/effects"
 
 const Style = styled.div`
 
 	width: 90%;
 
+	font-size: 14.5px;
+
 `
 
-const options = [
-	{ value: 'chocolate', label: 'Il' },
-	{ value: 'faut', label: 'Faut' },
-	{ value: 'mettre', label: 'Mettre' },
-	{ value: 'des', label: 'Des' },
-	{ value: 'trucs', label: 'Trucs' },
-	{ value: 'un', label: 'Un' },
-	{ value: 'jour', label: 'Jour' },
-	{ value: 'peut etre', label: 'Peut etre' }
+const userOptions = [
+	{ value: 'ilandols', label: 'ilandols' },
+	{ value: 'sbelabba', label: 'sbelabba' },
+	{ value: 'cchapon', label: 'cchapon' },
+	{ value: 'adouay', label: 'adouay' }
+]
+
+const channelOptions = [
+	{ value: 'public', label: 'public' },
+	{ value: 'protect', label: 'protect' },
+	{ value: 'protect', label: 'private' },
+]
+
+const groupOptions = [
+	{ label: 'users', options: userOptions},
+	{ label: 'channels', options: channelOptions}
 ]
 
 function SearchBar() {
@@ -24,65 +35,136 @@ function SearchBar() {
 		<Style>
 			<Select
 				placeholder={"Search..."}
-				options={options}
+				options={groupOptions}
 				styles={{
+					
 					// La barre en elle-meme
 					control: (props) => ({
 						...props,
+
 						border: "none",
-						backgroundColor: `${colors.section}`
+						boxShadow: "none",
+						
+						clipPath: effects.pixelateWindow,
+
+						backgroundColor: colors.section
+					}),
+
+					// La partie gauche de la barre (input donc)
+					valueContainer: (props) => ({
+						...props,
+
+						cursor: "text",
 					}),
 
 					// Le texte affiche par defaut
 					placeholder: (props) => ({
 						...props,
-						color: `${colors.text}`
+
+						color: colors.text
 					}),
 
 					// Le texte ecrit par l'utilisateur
 					input: (props) => ({
 						...props,
-						color: `${colors.text}`
+						
+						color: colors.text
 					}),
 
-					// L'option selectionnee
+					// L'option selectionnee affichee dans la barre
 					singleValue: (props) => ({
 						...props,
-						color: `${colors.text}`
+						
+						color: colors.text,
 					}),
-
-					// Les resultats de la recherche
-					option: (props, state) => ({
+					
+					// Le separateur du champ texte et du bouton
+					indicatorSeparator: (props) => ({
 						...props,
-						color: `${colors.text}`,
-						backgroundColor: `${state.isFocused ? colors.sectionHover : colors.section}`
+
+						display: "none"
 					}),
 
-					// Le conteneur des options
+					// Le bouton avec la fleche (remplacer par une loupe si possible)
+					dropdownIndicator: (props) => ({
+						...props,
+						
+						color: colors.text, '&:hover': {
+							color: colors.sectionHover
+						},
+
+						backgroundColor: colors.sectionAlt
+					}),
+
+					// Le menu en lui-meme
+					menu: (props) => ({
+						...props,
+
+						boxShadow: "none",
+
+						backgroundColor: "none"
+
+					}),
+					
+					// Le conteneur des options et des groupes
 					menuList: (props) => ({
 						...props,
-						color: `${colors.text}`,
-						backgroundColor: `${colors.section}`
+
+						padding: '0',
+
+						clipPath: effects.pixelateWindow
+					}),
+
+					// Le header d'un groupe
+					groupHeading: (props) => ({
+						...props,
+
+						display: "flex",
+						alignItems: "center",
+	
+						height: "35px",
+
+						margin: '0',
+						paddingLeft: "10px",
+
+						fontSize: "16px",
+
+						color: colors.text,
+						backgroundColor: colors.sectionAlt
+					}),
+
+					// Le groupe en lui-meme
+					group: (props) => ({
+						...props,
+
+						maxHeight: "140px",
+
+						padding: 0,
+
+						overflowY: "hidden"
+					}),
+
+					// L'option en elle-meme
+					option: (props, state) => ({
+						...props,
+
+						paddingLeft: "20px",
+
+						backgroundColor: state.isFocused ?
+							colors.sectionHover :
+							colors.section,
+							'&:active': {
+								backgroundColor: colors.reduceButtonHover
+							}
 					}),
 
 					// Le resultat affiche si aucune correspondance
 					noOptionsMessage: (props) => ({
 						...props,
-						color: `${colors.text}`
+
+						color: colors.text
 					}),
 
-					// Le bouton avec la fleche
-					dropdownIndicator: (props) => ({
-						...props,
-						color: `${colors.text}`,
-						backgroundColor: `${colors.sectionAlt}`
-					}),
-
-					// Le separateur du champ texte et du bouton
-					indicatorSeparator: (props) => ({
-						...props,
-						display: "none"
-					})
 				}}
 			/>
 		</Style>
