6b5ccb628114a1ddbf7770a1f891dd6514c209b2
il94 <ilyes.landolsi@outlook.fr>
2023-11-29 14:02:32 +0100

    feat: add LinkBUtton component

---
+++ front/src/componentsLibrary/LinkButton/index.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import colors from '../../utils/colors'
+import effects from '../../utils/effects'
+
+const LinkButton = styled(Link)`
+
+	padding-top: 1px;
+	padding-left: 11px;
+	padding-bottom: 3px;
+	padding-right: 13px;
+
+	cursor: pointer;
+
+	font-size: 35px;
+	text-decoration: none;
+	text-align: center;
+
+	${effects.shadowButton};
+	clip-path: ${effects.pixelateWindow};
+	
+	color: ${colors.text};
+	background-color: ${colors.button};
+
+	&:hover {
+		transform: scale(1.015);
+	}
+	&:active {
+		transform: scale(0.95);
+		transform: translate(2px, 2px);
+		background-color: ${colors.shadowButton};
+		border-color: ${colors.shadowButton};
+	}
+
+`
+
+export default LinkButton
