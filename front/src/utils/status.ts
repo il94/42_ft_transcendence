985639af2ef2c03816f97fadce65ea4b01073d47
il94 <ilyes.landolsi@outlook.fr>
2023-12-02 20:44:12 +0100

    feat: create status

--- front/src/utils/status.ts
+++ front/src/utils/status.ts
@@ -1,9 +1,15 @@
-enum status {
+export enum userStatus {
+	ONLINE = "En ligne",
+	OFFLINE = "Hors ligne",
+	PLAYING = "En jeu",
+	WAITING = "En recherche de partie",
+	WATCHING = "Spectateur"	
+}
+
+export enum challengeStatus {
 	PENDING = "pending",
 	ACCEPTED = "accepted",
 	CANCELLED = "cancelled",
 	IN_PROGRESS = "in progress",
 	FINISHED = "finished"	
 }
-
-export default status
