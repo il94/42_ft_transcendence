8754153ac1f13dd239680b59ac03419b14db5588
Claire CHAPON <cchapon@e3r3p1.clusters.42paris.fr>
2023-12-05 16:12:37 +0100

    to push

--- back/src/chats/chats.gateway.ts
+++ back/src/chats/chats.gateway.ts
@@ -1,11 +1,22 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer } from '@nestjs/websockets';
 import { ChatsService } from './chats.service';
 import { CreateChatDto, UpdateChatDto, MessageDto } from './dto';
-import { Body } from '@nestjs/common';
+import { Body, OnModuleInit } from '@nestjs/common';
+import { Server } from 'socket.io';
 
 @WebSocketGateway()
-export class ChatsGateway {
+export class ChatsGateway implements OnModuleInit {
   constructor(private readonly chatsService: ChatsService) {}
+
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('Connected');
+    })
+  }
 
   @SubscribeMessage('createChat')
   create(@MessageBody() createChatDto: CreateChatDto) {
