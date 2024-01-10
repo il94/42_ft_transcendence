a7ed0b4abd8919ae60fc271ad97319003fd8daa4
il94 <ilyes.landolsi@outlook.fr>
2024-01-10 20:55:03 +0100

    feat: handle owner user leave channel in frontend

--- front/src/components/Chat/Banner/index.tsx
+++ front/src/components/Chat/Banner/index.tsx
@@ -1,14 +1,25 @@
-import { Dispatch, SetStateAction, useContext } from "react"
-// import axios from "axios"
+import {
+	Dispatch,
+	SetStateAction,
+	useContext
+} from "react"
+import axios from "axios"
 
-import { ChannelName, ButtonsWrapper, Style, LeaveButtonWrapper } from "./style"
+import {
+	ChannelName,
+	ButtonsWrapper,
+	Style,
+	LeaveButtonWrapper
+} from "./style"
 
 import Icon from "../../../componentsLibrary/Icon"
 
 import ChatContext from "../../../contexts/ChatContext"
 import InteractionContext from "../../../contexts/InteractionContext"
+import AuthContext from "../../../contexts/AuthContext"
 
 import { channelStatus, chatWindowStatus } from "../../../utils/status"
+import { Channel, User } from "../../../utils/types"
 
 import LeaveIcon from "../../../assets/deconnexion.png"
 import ReduceIcon from "../../../assets/reduce.png"
@@ -23,28 +34,63 @@ type PropsBanner = {
 
 function Banner({ chatWindowState, setChatWindowState, bannerName, setErrorRequest }: PropsBanner) {
 
-	const { userAuthenticate, channelTarget, setChannelTarget } = useContext(InteractionContext)!
+	const { token } = useContext(AuthContext)!
+	const { userAuthenticate, setUserAuthenticate, channelTarget, setChannelTarget } = useContext(InteractionContext)!
 
 	async function leaveChannel() {
+
+		function getNewOwner(channel: Channel): User | undefined {
+
+			const adminFind: User | undefined = channel.administrators.find((administrator) => administrator.id !== channel.owner.id)
+			if (adminFind)
+				return (adminFind)
+			const memberFind: User | undefined = channel.users.find((user) => user.id !== channel.owner.id)
+			if (memberFind)
+				return (memberFind)
+			return (undefined)
+		}
+
 		try {
 			if (!channelTarget)
 				throw (new Error)
 
-			/* ============ Temporaire ============== */
+			if (channelTarget.users.length === 1)
+			{
+				await axios.delete(`http://localhost:3333/channel/${channelTarget.id}`, {
+					headers: {
+						'Authorization': `Bearer ${token}`
+					}
+				})
+				setChannelTarget(undefined)
+			}
+			else
+			{
+				/* ============ Temporaire ============== */
 
-			// await axios.delete(`http://localhost:3333/channel/${channelTarget.id}/members/${userAuthenticate.id}`)
+				// appeler la route qui supprime un user d'un channel
+				// await axios.delete(`http://localhost:3333/channel/${channelTarget.id}/members/${userAuthenticate.id}`)
 
-			/* ====================================== */
+				/* ====================================== */
 
-			channelTarget.users.splice(channelTarget.users.indexOf(userAuthenticate), 1)
+				setChannelTarget((prevState: Channel | undefined) => {
+					if (prevState)
+					{
+						return {
+							...prevState,
+							users: prevState.users.filter((user) => user.id !== userAuthenticate.id),
+							administrators: prevState.administrators.filter((administrator) => administrator.id !== userAuthenticate.id),
+							owner: prevState.owner.id === userAuthenticate.id ? getNewOwner(channelTarget)! : prevState.owner
+						}
+					}
+					else
+						return (undefined)
+				})
+			}
 
-			/* ============ Temporaire ============== */
-
-			// await axios.delete(`http://localhost:3333/user/me/channels/${userAuthenticate.id}`)
-
-			/* ====================================== */
-
-			userAuthenticate.channels.splice(userAuthenticate.channels.indexOf(channelTarget), 1)
+			setUserAuthenticate((prevState) => ({
+				...prevState,
+				channels: prevState.channels.filter((channel) => channel.id !== channelTarget.id)
+			}))
 
 			setChannelTarget(undefined)
 		}
