7491687522d7f399e6aa13b25a3d777162ebeced
il94 <ilyes.landolsi@outlook.fr>
2024-01-27 17:15:42 +0100

    feat: get token in Home page by cookies or local

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -33,11 +33,13 @@ function Home() {
 	const navigate = useNavigate();
 
 	useEffect(() => {
-		const access_token = Cookies.get('access_token')
-		if (access_token) {
+		const access_token: string | null | undefined = Cookies.get('access_token') ? Cookies.get('access_token') : localStorage.getItem('access_token')
+
+		if (access_token)
+		{
 			localStorage.setItem('access_token', access_token)
 			setToken(access_token)
-		}		
+		}	
 	}, [])
 
 	async function handleDeconnexionClickText() {
