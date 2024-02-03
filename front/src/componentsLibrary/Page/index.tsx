291be378b39e3350b19f56113a8c40ef94204de1
il94 <ilyes.landolsi@outlook.fr>
2024-02-03 16:16:23 +0100

    fix: overflow in auth pages (in progress)

--- front/src/componentsLibrary/Page/index.tsx
+++ front/src/componentsLibrary/Page/index.tsx
@@ -3,8 +3,7 @@ import styled from 'styled-components'
 const Page = styled.div`
 
 	display: flex;
-	flex-direction: column;
-	align-items: center;
+	justify-content: center;
 
 	min-height: 100%;
 
