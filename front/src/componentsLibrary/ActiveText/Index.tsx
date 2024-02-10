a29598b2783fd72f81774d08b30a147aa4f50b0e
il94 <ilyes.landolsi@outlook.fr>
2024-02-10 18:21:04 +0100

    feat: add color for focus alt text

--- front/src/componentsLibrary/ActiveText/Index.tsx
+++ front/src/componentsLibrary/ActiveText/Index.tsx
@@ -2,22 +2,28 @@ import styled from 'styled-components'
 
 import colors from '../../utils/colors'
 
-const ActiveText = styled.button <{ color?: string }>`
+const ActiveText = styled.button <{ color?: string, $fontSize?: number }>`
+
+	padding-right: 0;
+	padding-left: 0;
 
 	text-decoration: none;
+
+	font-size: ${(props) => props.$fontSize && props.$fontSize}px;
 
 	color: ${(props) => props.color};
 	background-color: inherit;
 
 	&:hover {
 		cursor: pointer;
+		color: ${(props) => props.color && props.color !== colors.text && colors.shadowButton};
 	}
 	&:active {
 		color: ${(props) => props.color && props.color !== colors.text && colors.shadowButton};
 	}
 	&:focus-visible {
 		outline: none;
-		color: ${colors.focusButton};
+		color: ${(props) => (props.color && props.color === colors.textAlt) ? colors.focusTextAlt : colors.focusText};
 	}
 
 `
