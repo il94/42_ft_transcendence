f2080347c474e89dfc59ea63d038fa25d904a2f9
il94 <ilyes.landolsi@outlook.fr>
2024-02-10 20:24:54 +0100

    fix: http to https

--- back/src/auth/services/auth.service.ts
+++ back/src/auth/services/auth.service.ts
@@ -175,7 +175,7 @@ export class AuthService {
 					const token = await this.signToken(user.id, user.username)
 						res.clearCookie('token', { httpOnly: true })
 						.cookie("access_token", token.access_token)
-						.redirect(`http://${process.env.IP}:5173`)
+						.redirect(`https://${process.env.IP}:5173`)
 				}
 			
 				// Si le user a active la twoFA
@@ -184,7 +184,7 @@ export class AuthService {
 					// Redirige vers la page twoFA avec les infos necessaires pour le front
 					res.cookie('two_FA', true)
 					.cookie('userId', user.id)
-					.redirect(`http://${process.env.IP}:5173/twofa`)	
+					.redirect(`https://${process.env.IP}:5173/twofa`)	
 				}
 			}
 			// Si le user ne possede pas de compte dans l'app
@@ -195,12 +195,12 @@ export class AuthService {
 				const fiveMin = Date.now() + 5 * 60 * 1000;
 				res.cookie('usernameId', user.usernameId, { expires: new Date(fiveMin), /*httpOnly: true*/ })
 				.cookie("avatar", user.avatar, { expires: new Date(fiveMin) })
-				.redirect(`http://${process.env.IP}:5173/signup42`)
+				.redirect(`https://${process.env.IP}:5173/signup42`)
 			}
 		}
 		catch (error) {
 			res.cookie("error_message", error.message).
-			redirect(`http://${process.env.IP}:5173/error`)
+			redirect(`https://${process.env.IP}:5173/error`)
 		}
 	}
 
