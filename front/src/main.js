bd21fb85e7895470a3d740d9d169ccdee710db4b
Claire CHAPON <cchapon@e3r3p1.clusters.42paris.fr>
2024-02-13 14:57:35 +0100

    run dev

---
+++ front/src/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = require("react");
+var ReactDOM = require("react-dom/client");
+var App_1 = require("./App");
+ReactDOM.createRoot(document.getElementById('root')).render(<react_1.default.StrictMode>
+		<App_1.default />
+	</react_1.default.StrictMode>);
