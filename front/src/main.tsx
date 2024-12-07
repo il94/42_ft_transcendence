4f9093722a58692c45e4d5346dca37bce584e590
il94 <ilyes.landolsi@outlook.fr>
2024-12-07 15:33:55 +0100

    build: remove unused dependencies && downgrade react

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,7 +1,6 @@
-import * as ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<App />
-)
+ReactDOM.render(<App />, document.getElementById('root'));
+
